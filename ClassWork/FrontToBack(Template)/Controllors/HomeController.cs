73ae20cb472d9faf527442ef3aa60682784776c3
HuseynJafarov <huseynnj@code.edu.az>
2022-10-04 05:57:22 +0400

    FrontToBack

---
+++ ClassWork/FrontToBack(Template)/Controllors/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack_Template_.Controllors
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewData["numbers"] = new int[] {1,2,3,4,5,6,7,8,9 };
+            ViewBag.nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            return View();
+        }
+    }
+}
